e9f44a6561f80928ddb74220817b201cc5109b49
jairo.mendez <jairo.mendez@grupoonetec.com>
2022-04-27 17:12:18 +0200

    create new chapter 02_iterationsElement

---
+++ cypress/integration/common/stepDefinitions/iterations.js
@@ -0,0 +1,24 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+
+Given(`User configure the execution before the scenarios`, () => {
+	console.log("User configure the execution before the scenarios")
+});
+
+
+Given(`User navigates to url with delay`, () => {
+	// cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=3000') // sigue funcionando con 3 segundos
+	cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000') // No funcionaría por defecto con 5 segundos debido a que Cypress espera 4 segundos por elemento.
+});
+
+When(`User does double click over option {string}`, (option) => {
+	cy.get('.new-todo', {timeout: 6000}).type(`${option}{enter}`); //funcionará aumentando el timeout del objeto
+	cy.contains(option).dblclick();
+	cy.get(`input[value='${option}'][class='edit']`, {timeout: 6000}).type(`{enter}`);
+});
+
+Given(`User does mouse over {string}`, (option) => {
+	cy.xpath(`//*[text()='${option}']/following-sibling::button[@class='destroy']`).should('not.be.visible'); //Comprobando que no es visible
+	cy.xpath(`//*[text()='${option}']/following-sibling::button[@class='destroy']`).invoke('show'); //invocando la visibilidad del elemento
+	cy.xpath(`//*[text()='${option}']/following-sibling::button[@class='destroy']`).should('be.visible'); //Comprobando que es visible
+});
---
+++ cypress/integration/features/iterations.feature
@@ -0,0 +1,12 @@
+Feature: Iterations
+	as a user
+	I want to test differents iterations
+
+	Background:
+		Given User configure the execution before the scenarios
+
+	@iterations
+	Scenario: Test differents interactions 
+		Given User navigates to url with delay
+		Then User does double click over option "secondStep"
+		And User does mouse over "secondStep"
